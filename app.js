4736342e03d3adcb699340933d5fbbf208ef2188
Prabor Mukherjee <prabor1@gmail.com>
2021-03-13 21:12:37 +0530

    added pug as templating engine

--- app.js
+++ app.js
@@ -5,17 +5,21 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'pug');   //find the dynamic engine named pug
+app.set('views', 'views');   //where to find those templetes
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);    // go to /admin/add-product
+app.use('/admin', adminData.routes);    // go to /admin/add-product
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
