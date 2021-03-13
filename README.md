327f4f8b8e8b5a402a520f2fc47d6c9465f2e4c9
Prabor Mukherjee <prabor1@gmail.com>
2021-03-13 21:33:47 +0530

    added hbs as templating engine

--- README.md
+++ README.md
@@ -1,25 +1,20 @@
-# node104
+# node105
 
 Next part of application.  
 
-Added dynamic `templating engine(pug)` in place of `html` the `views` folder.  
+Added dynamic `templating engine(handlebars)` in place of `pug` the `views` folder.  
 
 + `Shop` for main `home` page  
 + `add-product` for `adding product`  
 + `404` for `Page Not Found`  
 + `main-layout` for common parts from all pages(in `/layouts`)  
 
-Added styling in the `public/css`
-
-+ updates all previous stylings  
-+ `forms` for form(html) styling  
-
-Desc:
+Desc:  
 `app.js` is the main driver code.  
-all routes related logic is in the `routes` folder(`shop.js`, `admin.js`)  
-all `pug` codes in the `views` folder(`404.pug`, `add-product.pug`, `shop.pug`)
-all css codes in `public/css` folder(`main.css`, `product.css`, `forms.css`)
+all `routes` related logic is in the `routes` folder(`shop.js`, `admin.js`)  
+all `handlebars` codes in the `views` folder(`404.hbs`, `add-product.hbs`, `shop.hbs`)  
+all `css` codes in `public/css` folder(`main.css`, `product.css`, `forms.css`)
 
 To run:  
 `npm i` or `npm install`  
-`npm start` or `nodemon app.js` or `node app.js`
+`npm start` or `nodemon app.js` or `node app.js`  
--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.set('view engine', 'pug');   //find the dynamic engine named pug
-app.set('views', 'views');   //where to find those templetes
+app.engine(
+  "hbs",
+  expressHbs({
+    layoutsDir: "views/layouts/",
+    defaultLayout: "main-layout",
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs"); //using handlebars engine as hbs
+app.set("views", "views"); //where to find those templetes
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminData.routes);    // go to /admin/add-product
+app.use("/admin", adminData.routes); // go to /admin/add-product
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
