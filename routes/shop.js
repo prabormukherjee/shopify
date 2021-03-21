9fc5b345ded4fafab8c26300544667abdae2ed64
Prabor Mukherjee <prabor1@gmail.com>
2021-03-21 21:04:36 +0530

    added ejs as templating engine

--- routes/shop.js
+++ routes/shop.js
@@ -1,24 +1,23 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const adminData = require('./admin');
-const rootDir = require('../util/path');
+const adminData = require("./admin");
+const rootDir = require("../util/path");
 
 const Router = express.Router();
 
-
-Router.get('/', (req, res, next) => {
-    // console.log(adminData.products);
-    const products = adminData.products
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop', 
-        path: '/', 
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
-    });
+Router.get("/", (req, res, next) => {
+  // console.log(adminData.products);
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
-module.exports =  Router;
+module.exports = Router;
